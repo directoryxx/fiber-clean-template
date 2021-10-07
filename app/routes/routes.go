828b49e206e7f63a71be635d59e91f3e42ad0ef9
directoryx <anggawijaya4567@gmail.com>
2021-10-07 17:08:41 +0700

    #13 - init swagger

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -2,7 +2,10 @@ package routes
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
 
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/casbin/casbin/v2"
 	"github.com/directoryxx/fiber-clean-template/app/controller"
 	"github.com/directoryxx/fiber-clean-template/app/interfaces"
@@ -18,6 +21,23 @@ func RegisterRoute(app *fiber.App, ctx context.Context, log interfaces.Logger, e
 	HomeController := controller.NewHomeController(log, app)
 	RoleController := controller.NewRoleController(log, app, enforcer)
 	PermissionController := controller.NewPermissionController(log, enforcer, app)
+
+	app.Get("/docs/*", swagger.New(
+		swagger.Config{
+			URL:         os.Getenv("APP_URL") + "/swagger.json",
+			DeepLinking: false,
+			// Expand ("list") or Collapse ("none") tag groups by default
+			DocExpansion: "none",
+		},
+	)) // default
+
+	app.Get("/swagger.json", func(c *fiber.Ctx) error {
+		body, err := ioutil.ReadFile("docs/swagger.json")
+		if err != nil {
+			panic(err)
+		}
+		return c.SendString(string(body))
+	})
 
 	app.Get("/dashboard", monitor.New())
 
